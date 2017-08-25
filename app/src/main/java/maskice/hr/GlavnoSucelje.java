96a5f50f586ed47b812b1b031760b84613c413ac
markoidzan <marko.idzan@gmail.com>
2017-08-25 13:32:48 +0200

    Some tweaks
    Tryings out of some crap

--- app/src/main/java/maskice/hr/GlavnoSucelje.java
+++ app/src/main/java/maskice/hr/GlavnoSucelje.java
@@ -11,8 +11,10 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.ExpandedMenuView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ExpandableListAdapter;
 
 
 public class GlavnoSucelje extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
